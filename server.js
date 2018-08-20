2ea7d2f4d6be924327eaccbb31020b9e8259e8ee
Jeff Rafter <jeffrafter@github.com>
2018-08-20 14:47:01 -0700

    It's cool

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3001
 
 
 app.get('/', (req, res) => {
-  console.log("=>>>>>>>>>> cooler kids")
+  console.log("=>>>>> cooler kids")
   res.sendfile('./index.html')
 })
 
