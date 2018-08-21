86d659303d00e293f47a659d9c8a791412075b06
Jeff Rafter <jeffrafter@github.com>
2018-08-20 21:58:40 -0700

    They are cooler

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3001
 
 
 app.get('/', (req, res) => {
-  console.log("=>>>>> cooler kids")
+  console.log("=>>>>> cooler kids are cooler")
   res.sendfile('./index.html')
 })
 
