c1e1fae47374e77be04eb4e147976235ef7d5917
Jeff Rafter <jeffrafter@github.com>
2018-09-06 12:41:49 -0700

    CSS and JS

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3001
 
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   console.log("=>>>>> cooler kids")
